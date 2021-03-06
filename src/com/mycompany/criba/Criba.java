e1b72f925f75e2e14031a6b0d22d0a3f01a54d01
LuisDavidAlejo <73552173+LuisDavidAlejo@users.noreply.github.com>
2021-03-06 20:36:30 +0100

    Rellenar Javadoc, las pruebas parametrizadas dan error

--- src/com/mycompany/criba/Criba.java
+++ src/com/mycompany/criba/Criba.java
@@ -23,51 +23,74 @@ package com.mycompany.criba;
 * @author Fernando Berzal
 * @version 1.0 Enero’2005 (FB)
 */
-public class Criba {
-	/**
-	* Generar numeros primos de 1 a max
-	* @param max es el valor maximo
-	* @return Vector de numeros primos
-	*/
-	public static int[] generarPrimos (int max) {
-		int i,j;
-		if (max >= 2) {
-			// Declaraciones
-			int dim = max + 1; // Tamanho del array
-			boolean[] esPrimo = new boolean[dim];
-			
-			// Inicializar el array
-			for (i=0; i<dim; i++)
-				esPrimo[i] = true;
-			
-			// Eliminar el 0 y el 1, que no son primos
-			esPrimo[0] = esPrimo[1] = false;
-			
-			// Criba
-			for (i=2; i<Math.sqrt(dim)+1; i++) {
-				if (esPrimo[i]) {
-					// Eliminar los multiplos de i
-					for (j=2*i; j<dim; j+=i)
-					esPrimo[j] = false;
-				}
-			}
-			
-			// ¿Cuantos primos hay?
-			int cuenta = 0;
-			for (i=0; i<dim; i++) {
-				if (esPrimo[i])
-				cuenta++;
-			}
-			
-			// Rellenar el vector de numeros primos
-			int[] primos = new int[cuenta];
-			for (i=0, j=0; i<dim; i++) {
-				if (esPrimo[i])
-				primos[j++] = i;
-			}
-			return primos;
-		} else { // max < 2
-			return new int[0]; // Vector vacio
+
+public class Criba
+{
+	private static int dim;
+	private static boolean esPrimo[];
+	private static int primos[];
+	
+	public static int[] generarPrimos (int max)
+	{
+		if (max < 2) 
+		{
+			return new int[0];   //vector vacio
 		}
-	}	
+		else
+		{
+			inicializarCriba (max);
+			realizarCriba();
+			rellenarPrimos();
+			return primos;
+		}
+	}
+	
+	private static void inicializarCriba (int max)
+	{
+		int i;
+		dim = max + 1;
+		esPrimo = new boolean [dim];
+		for (i=0; i<dim; i++)
+			esPrimo[i] = true;
+		esPrimo[0] = esPrimo[1] = false;
+	}
+	
+	private static void realizarCriba ()
+	{
+		int i,j;
+		for (i=2; i<Math.sqrt(dim)+1; i++)
+		{
+			if (esPrimo[i])
+			{
+				//Eliminar los multiplos de i
+				for (j=2*i; j<dim; j+=1)
+					esPrimo[j] = false;
+			}
+		}
+	}
+	
+	private static void rellenarPrimos()
+	{
+		int i, j, cuenta;
+		//Cuantos primos hay?
+		cuenta = 0;
+		for (i=0; i<dim; i++)
+		{
+			if (esPrimo[i])
+				cuenta++;
+			// Rellenar el vector de numeros primos
+			primos = new int[cuenta];
+			for (i=0, j=0; i<dim; i++)
+				if (esPrimo[i])
+					primos [j++] = i;
+		}
+		
+	}
 }
+	
+
+
+
+
+
+
