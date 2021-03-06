fb444ff2214d14673c0891f459adf15073cea6ce
LuisDavidAlejo <73552173+LuisDavidAlejo@users.noreply.github.com>
2021-03-06 02:05:50 +0100

    Quitado acentos ycambiado la ñ por la nh para que no haya problemas

--- src/com/mycompany/criba/Criba.java
+++ src/com/mycompany/criba/Criba.java
@@ -1,39 +1,39 @@
 
 package com.mycompany.criba;
 /**
-* Clase para generar todos los números primos de 1 hasta
-* un número máximo especificado por el usuario. Como
-* algoritmo se utiliza la criba de Eratóstenes.
+* Clase para generar todos los numeros primos de 1 hasta
+* un numero maximo especificado por el usuario. Como
+* algoritmo se utiliza la criba de Eratostenes.
 * <p>
-* Eratóstenes de Cirene (276 a.C., Cirene, Libia – 194
-* a.C., Alejandría, Egipto) fue el primer hombre que
-* calculó la circunferencia de la Tierra. También
+* Eratostenes de Cirene (276 a.C., Cirene, Libia – 194
+* a.C., Alejandria, Egipto) fue el primer hombre que
+* calculo la circunferencia de la Tierra. Tambien
 * se le conoce por su trabajo con calendarios que ya
-* incluían años bisiestos y por dirigir la mítica
-* biblioteca de Alejandría.
+* incluian anhos bisiestos y por dirigir la mitica
+* biblioteca de Alejandria.
 * <p>
 * El algoritmo es bastante simple: Dado un vector de
-* enteros empezando en 2, se tachan todos los múltiplos
-* de 2. A continuación, se encuentra el siguiente
-* entero no tachado y se tachan todos sus múltiplos. El
-* proceso se repite hasta que se pasa de la raíz cuadrada
-* del valor máximo. Todos los números que queden sin
-* tachar son números primos.
+* enteros empezando en 2, se tachan todos los multiplos
+* de 2. A continuacion, se encuentra el siguiente
+* entero no tachado y se tachan todos sus multiplos. El
+* proceso se repite hasta que se pasa de la raiz cuadrada
+* del valor maximo. Todos los numeros que queden sin
+* tachar son numeros primos.
 *
 * @author Fernando Berzal
 * @version 1.0 Enero’2005 (FB)
 */
 public class Criba {
 	/**
-	* Generar números primos de 1 a max
-	* @param max es el valor máximo
-	* @return Vector de números primos
+	* Generar numeros primos de 1 a max
+	* @param max es el valor maximo
+	* @return Vector de numeros primos
 	*/
 	public static int[] generarPrimos (int max) {
 		int i,j;
 		if (max >= 2) {
 			// Declaraciones
-			int dim = max + 1; // Tamaño del array
+			int dim = max + 1; // Tamanho del array
 			boolean[] esPrimo = new boolean[dim];
 			
 			// Inicializar el array
@@ -46,20 +46,20 @@ public class Criba {
 			// Criba
 			for (i=2; i<Math.sqrt(dim)+1; i++) {
 				if (esPrimo[i]) {
-					// Eliminar los múltiplos de i
+					// Eliminar los multiplos de i
 					for (j=2*i; j<dim; j+=i)
 					esPrimo[j] = false;
 				}
 			}
 			
-			// ¿Cuántos primos hay?
+			// ¿Cuantos primos hay?
 			int cuenta = 0;
 			for (i=0; i<dim; i++) {
 				if (esPrimo[i])
 				cuenta++;
 			}
 			
-			// Rellenar el vector de números primos
+			// Rellenar el vector de numeros primos
 			int[] primos = new int[cuenta];
 			for (i=0, j=0; i<dim; i++) {
 				if (esPrimo[i])
@@ -67,7 +67,7 @@ public class Criba {
 			}
 			return primos;
 		} else { // max < 2
-			return new int[0]; // Vector vacío
+			return new int[0]; // Vector vacio
 		}
 	}	
 }
